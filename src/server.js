cf361b5d9185e92627e0af22989daab862f95670
Guilherme Politano <gui_politano@hotmail.com>
2019-10-27 09:33:06 -0300

    Adiciona tratamento de erro 404

--- src/server.js
+++ src/server.js
@@ -5,4 +5,12 @@ const app = express();
 const porta = process.env.PORT || 8080;
 
 app.use(cors(),routes);
+
+app.use(function(req, res, next) {
+    res.status(404).send({
+        "message": "Rota não encontrada, olhe a documentação para ver as rotas disponíveis",
+        "data": []
+    });
+});
+
 app.listen(porta);
